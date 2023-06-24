e509e68efd5c492716f77d5ec55829f85e94cf90
Trinh Quang Minh <minh15553@gmail.com>
2023-06-24 23:41:11 +0700

    init project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from server side!')
+})
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...')
+})
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`)
+})
